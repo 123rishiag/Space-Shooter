718964530452a517d3eeb9c4dbec4a5b6e991410
Rishabh Garg <narishabh.garg@gmail.com>
2024-12-09 03:07:32 +0530

    Fixed some naming conventions

--- Assets/Scripts/PowerUps/PowerUpPool.cs
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -1,27 +1,29 @@
 using System;
 using CosmicCuration.Utilities;
-using CosmicCuration.PowerUps;
 
-public class PowerUpPool : GenericObjectPool<IPowerUp>
+namespace CosmicCuration.PowerUps
 {
-    private PowerUpData dataToUse;
-
-    public IPowerUp GetPowerUp<T>(PowerUpData powerUpData) where T : IPowerUp
+    public class PowerUpPool : GenericObjectPool<IPowerUp>
     {
-        dataToUse = powerUpData;
-        return GetItem<T>();
-    }
+        private PowerUpData dataToUse;
 
-    protected override IPowerUp CreateItem<T>()
-    {
-        if (typeof(T) == typeof(Shield))
-            return new Shield(dataToUse);
-        else if (typeof(T) == typeof(DoubleTurret))
-            return new DoubleTurret(dataToUse);
-        else if (typeof(T) == typeof(RapidFire))
-            return new RapidFire(dataToUse);
-        else
-            throw new NotSupportedException($"Power-up type '{typeof(T)}' is not supported.");
-    }
+        public IPowerUp GetPowerUp<T>(PowerUpData powerUpData) where T : IPowerUp
+        {
+            dataToUse = powerUpData;
+            return GetItem<T>();
+        }
 
+        protected override IPowerUp CreateItem<T>()
+        {
+            if (typeof(T) == typeof(Shield))
+                return new Shield(dataToUse);
+            else if (typeof(T) == typeof(DoubleTurret))
+                return new DoubleTurret(dataToUse);
+            else if (typeof(T) == typeof(RapidFire))
+                return new RapidFire(dataToUse);
+            else
+                throw new NotSupportedException($"Power-up type '{typeof(T)}' is not supported.");
+        }
+
+    }
 }
